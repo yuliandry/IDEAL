d25419170d777e060084ec2691a562506159dcfa
yuliandry.fm <yuliandry.fm@gmail.com>
2023-08-12 16:07:39 +0700

    Initial commit

---
+++ src/main/java/dinsos/dtkp/ideal/IdealApplication.java
@@ -0,0 +1,13 @@
+package dinsos.dtkp.ideal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdealApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dinsos/dtkp/ideal/IdealApplicationTests.java
@@ -0,0 +1,13 @@
+package dinsos.dtkp.ideal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
